f0c066f31f73d1605ac149aeb4b3384cdd4f4fa5
Len Greski <len@greskilabs.com>
2022-08-20 12:09:07 -0400

    completed presentation, code to replicate survey weighting analysis with flat files

--- R/08-calculateStandardErrors.R
+++ R/08-calculateStandardErrors.R
@@ -1,13 +1,13 @@
 #
 # 08-calculateStandardErrors.R
 
-ga_pums_rep_weights <- get_pums(
+system.time(ga_pums_rep_weights <- get_pums(
   variables = c("PUMA", "SEX", "AGEP", "SCHL"),
   state = "GA",
   survey = "acs5",
   year = 2019,
   recode = TRUE,
-  rep_weights = "person")
+  rep_weights = "person"))
 
 format(object.size(ga_pums_rep_weights),unit="Mb")
 
@@ -19,12 +19,22 @@ library(srvyr)
 ga_survey_design <- to_survey(ga_pums_rep_weights)
 
 ga_survey_design %>% 
+  mutate(SEX_label = factor(SEX,labels = c("Male","Female"),ordered=TRUE)) %>% 
   survey_count(PUMA, SEX_label) -> ga_counts_se
 head(ga_counts_se)
 
 # now replicate process w/ flat file
+system.time(if(!file.exists("./data/Georgia/psam_p13.csv")){
+  download.file("https://www2.census.gov/programs-surveys/acs/data/pums/2019/5-Year/csv_pga.zip",
+                "./data/csv_pga.zip",
+                method="curl",
+                mode="wb")
+  unzip("./data/csv_pga.zip",exdir="./data/Georgia")
+})
 library(readr)
-system.time(ga_pums_rep_weights_file  <- read_csv("./data/Georgia/psam_p13.csv",col_names = TRUE))
+system.time(ga_pums_rep_weights_file  <- 
+              read_csv("./data/Georgia/psam_p13.csv",col_names = TRUE)
+)
 format(object.size(ga_pums_rep_weights_file),unit="Mb")
 
 ga_survey_design_file <- to_survey(ga_pums_rep_weights_file)
